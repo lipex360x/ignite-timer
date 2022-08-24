d3f1c2be236030f80bd8798a6b76cd386fdf77a6
Felipe Bambirra <lipex360@gmail.com>
2022-08-23 22:48:59 -0300

    history list updated

--- src/components/organisms/HistoryList/index.tsx
+++ src/components/organisms/HistoryList/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Heading } from '@/components/atoms/Heading'
 import { StatusBadge } from '@/components/atoms/StatusBadge'
 import { useCycleContext } from '@/contexts/CycleContext'
@@ -24,7 +26,12 @@ export const HistoryList = () => {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>
                   {cycle.finishedDate && <StatusBadge type="finalizado" />}
                   {cycle.interruptedDate && <StatusBadge type="interrompido" />}
